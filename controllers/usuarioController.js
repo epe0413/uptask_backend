7af4710b5b6c857636ccb68a785d29ef603b9bdc
Everth Paredes <epe0413@gmail.com>
2022-07-02 22:12:10 -0600

    Se agrego autenticación y validación de la misma en su respectivo endpoint

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -20,6 +20,35 @@ const registrar = async (req, res)=>{
     } catch (error) {
         console.log(error)
     }
+};
+
+const autenticar = async (req, res) => {
+    const { email, password} = req.body;
+    // Comprobar si el usuario existe
+    const usuario = await Usuario.findOne({email});
+    if(!usuario){
+        const error = new Error('El usuario no existe');
+        return res.status(404).json({msg: error.message})
+    }
+
+    // Comprobar que el usuario este comprobado
+    if(!usuario.confirmado){
+        const error = new Error('Cuenta no a sido confirmada');
+        return res.status(403).json({msg: error.message})
+    }
+    
+    // Comprobar su password password
+    if(await usuario.comprobarPassword(password)){
+        res.json({
+            _id: usuario._id,
+            nombre: usuario.nombre,
+            email: usuario.email
+        })
+    } else {
+        const error = new Error('El Password es incorrecto');
+        return res.status(403).json({msg: error.message})
+    }
+    
 }
 
-export { registrar };
+export { registrar, autenticar };
--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -2,9 +2,10 @@ import express from 'express'
 
 const router = express.Router();
 
-import { registrar } from '../controllers/usuarioController.js';
+import { registrar, autenticar } from '../controllers/usuarioController.js';
 
 // Autenticación, registro y confirmación de usuarios
 router.post('/', registrar); //Crea un nuevo usuario
+router.post('/login', autenticar); //Login
 
 export default router;
