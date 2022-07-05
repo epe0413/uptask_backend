d10bba04f30eeca0e200dd21577940a6314a777b
Everth Paredes <epe0413@gmail.com>
2022-07-05 15:31:01 -0600

    Se agrego funcionalidad para resetear password en el caso de olvido

--- routes/usuarioRoutes.js
+++ routes/usuarioRoutes.js
@@ -2,11 +2,13 @@ import express from 'express'
 
 const router = express.Router();
 
-import { registrar, autenticar, confirmar } from '../controllers/usuarioController.js';
+import { registrar, autenticar, confirmar, olvidePassword, comprobarToken } from '../controllers/usuarioController.js';
 
 // Autenticación, registro y confirmación de usuarios
 router.post('/', registrar); //Crea un nuevo usuario
 router.post('/login', autenticar);
-router.get('/confirmar/:token', confirmar)
+router.get('/confirmar/:token', confirmar);
+router.post('/olvide-password', olvidePassword);
+router.get('/olvide-password/:token', comprobarToken)
 
 export default router;
