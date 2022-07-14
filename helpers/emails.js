411dd40094112c7d300221c3e3f5c915ebe399d9
Everth Paredes <epe0413@gmail.com>
2022-07-14 15:41:25 -0600

    Se agrego funcionalidad para recetear el password en cso que se le haya olvidado, y se envia correo de confirmacion

--- helpers/emails.js
+++ helpers/emails.js
@@ -29,3 +29,35 @@ export const emailRegistro = async (datos) => {
         `,
     })
 }
+
+export const emailOlvidePassword = async (datos) => {
+    const { email, nombre, token } = datos
+
+    // TODO: Mover acia variables de entorno
+
+    const transport = nodemailer.createTransport({
+        host: "smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "82578c622e0c60",
+          pass: "6ca8c9641b58ec"
+        }
+    });
+
+    // Información del Email
+
+    const info = await transport.sendMail({
+        from: '"UpTask - Administrador de Proyectos" <cuentas@uptask.com>',
+        to: email,
+        subject: "UpTask - Reestable tu Password",
+        text: "Reestable tu Password",
+        html: `<p>Hola: ${nombre} has solicitado reestablecer tu password</p>
+        <p>Sigue el siguiente enlace para generar un nuevo password:
+
+        <a href="${process.env.FRONTEND_URL}/olvide-password/${token}">Reestabler Password</a>
+        
+        <p>Si tu no solicitaste este cambio, puedes ignorar este mensaje</p>
+        
+        `,
+    })
+}
